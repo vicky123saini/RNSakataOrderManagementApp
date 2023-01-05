d3351b1cbe7bfc6c380a23cce930b00b84904431
vikas saini <vicky123.saini@gmail.com>
2023-01-05 14:32:17 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,4 @@
-//export const BASE_URL = "http://205.147.109.122/bm.sid.service.dev/";
-export const BASE_URL = "http://182.18.139.97/BM.Service.SID.LIVE/";
+export const BASE_URL = "http://000.000.000.000/BM.Service.SID.LIVE/";
 export const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
